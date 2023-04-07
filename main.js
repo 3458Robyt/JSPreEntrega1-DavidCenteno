0c025947e82f43b075b77e5d64cfcbe0dbb6cdc7
David Centeno <centeno70204@gmail.com>
2023-04-07 18:47:36 -0500

    a2

--- main.js
+++ main.js
@@ -4,20 +4,29 @@ function isStrongPassword(contraseña) {
   return regex.test(contraseña);
 }
 
-function generarContraseña(length) {
+function generatePassword(length) {
   const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-={}[]|;:<>,.?/~';
   let contraseña = '';
   
-// Genera una contraseña aletoria hasta que una sea aprobada por la funcoin isStrongPassword
+// Genera una contraseña aletoria hasta que una sea aprobada por la funcionn isStrongPassword
 do {
+  contraseña='';
   for (let i = 0; i < length; i++) {
     const index = Math.floor(Math.random() * chars.length);
-    contraseña += chars[index];
+    contraseña += chars[index];  
   }
+  
 } while (!isStrongPassword(contraseña));
   //   Devuelve la contraseña generada
     alert(contraseña);
 }
 
 
-generarContraseña(prompt("Ingrese el largo de la contraseña, con un minimo de 8 caracteres de largo"));
+
+let largo = parseInt(prompt("Ingrese el largo de la contraseña, con un minimo de 8 caracteres de largo"));
+if (largo<=7) {
+  largo=0;
+  largo = parseInt(prompt("Ingrese un mayor largo de contraseña, con un minimo de 8 caracteres de largo"));
+}
+generatePassword(largo);  
+
